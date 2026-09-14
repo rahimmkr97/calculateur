996e03a9bbf1122c9ecb9679e6a0471b1df776c2
rahimmkr97 <rockb8585@gmail.com>
2026-09-14 08:31:26 +0100

    iphone

---
+++ README.txt
@@ -0,0 +1,12 @@
+FILM CALCULATOR PWA
+
+Upload all files in this folder to an HTTPS static host.
+
+On iPhone:
+1. Open the HTTPS site in Safari.
+2. Tap Share.
+3. Tap Add to Home Screen.
+4. Leave Open as Web App enabled if shown.
+5. Tap Add.
+
+The custom Film Calculator icon is included.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="film-calculator-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./icon-1024.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
